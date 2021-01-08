3f0d117b940ba15a2e3e8b233012e6e80f1f3b98
Hendrik Mans <hendrik@mans.de>
2021-01-08 17:23:54 +0100

    Docs

--- src/util/registerComponent.ts
+++ src/util/registerComponent.ts
@@ -1,3 +1,9 @@
+/**
+ * Convenience method to create a new custom element.
+ *
+ * @param name Name of the custom element to create
+ * @param constructor Class that will handle this element
+ */
 export const registerComponent = (name: string, constructor: CustomElementConstructor) => {
   if (customElements.get(name)) return
   customElements.define(name, constructor)
