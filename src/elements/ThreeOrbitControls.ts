55fb04bfc38b924b7ad074131057e380303e44fe
Hendrik Mans <hendrik@mans.de>
2021-01-11 12:43:04 +0100

    Clean up ThreeOrbitControls

--- src/elements/ThreeOrbitControls.ts
+++ src/elements/ThreeOrbitControls.ts
@@ -1,14 +1,12 @@
-import { ThreeElement } from "../ThreeElement"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { Scene } from "three"
-import { ThreeScene } from "./ThreeScene"
+import { ThreeElement } from "../ThreeElement"
 
 export class ThreeOrbitControls extends ThreeElement<OrbitControls> {
   controls?: OrbitControls
 
   readyCallback() {
-    const { renderer } = this.game!
-    const { camera } = this.scene!
+    const { renderer } = this.game
+    const { camera } = this.scene
     this.controls = new OrbitControls(camera, renderer.domElement)
   }
 
