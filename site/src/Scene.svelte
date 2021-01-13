eb45f6ef1505d012205ca05b6368d5e8c363a8df
Hendrik Mans <hendrik@mans.de>
2021-01-13 17:35:19 +0100

    Svelte bug #12442 :(

--- site/src/Scene.svelte
+++ site/src/Scene.svelte
@@ -7,6 +7,6 @@
   <three-ambient-light intensity="0.2" />
   <three-directional-light intensity="0.8" position="10, 10, 50" />
 
-  <Thingy />
+  <Thingy scale={5} />
   <three-orbit-controls />
 </three-scene>
--- site/src/Thingy.svelte
+++ site/src/Thingy.svelte
@@ -1,16 +1,22 @@
 <script>
+  export let speed = 1
+
+  console.log("my scope:", this)
+
   function rotate(dt) {
-    this.object.rotateZ(3 * dt)
+    this.object.rotateZ(speed * dt)
     this.game.requestFrame()
   }
 
   function rotateDeconstruct(dt, { object, game }) {
-    object.rotateZ(3 * dt)
+    console.log("rotateDeconstruct scope", this)
+
+    object.rotateZ(speed * dt)
     game.requestFrame()
   }
 
   const rotateArrow = (dt, { object, game }) => {
-    object.rotateZ(3 * dt)
+    object.rotateZ(speed * dt)
     game.requestFrame()
   }
 
@@ -20,7 +26,7 @@
 </script>
 
 <!-- Mesh -->
-<three-mesh scale="2.5" onupdate={rotateDeconstruct}>
+<three-mesh onupdate={rotateDeconstruct} {...$$restProps}>
   <three-dodecahedron-buffer-geometry />
   <three-mesh-standard-material color="hotpink" />
 </three-mesh>
