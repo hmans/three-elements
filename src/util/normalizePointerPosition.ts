79c4c3d59c17a54d4def6792772015f40233392d
Hendrik Mans <hendrik@mans.de>
2021-01-16 18:46:34 +0100

    Make the mouse coordinate normalization smarter
    (so it also works in non-fullsize viewports)

--- src/util/normalizePointerPosition.ts
+++ src/util/normalizePointerPosition.ts
@@ -8,8 +8,13 @@ export const normalizePointerPosition = (
 ) => {
   if (!target) target = new Vector2()
 
-  target.x = (x / renderer.domElement.clientWidth) * 2 - 1
-  target.y = -(y / renderer.domElement.clientHeight) * 2 + 1
+  const rect = renderer.domElement.getBoundingClientRect()
+
+  x -= rect.left
+  y -= rect.top
+
+  target.x = (x / rect.width) * 2 - 1
+  target.y = -(y / rect.height) * 2 + 1
 
   return target
 }
