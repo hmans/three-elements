f253d24af4d0f2aa75b42460b1132d43a56c4563
Hendrik Mans <hendrik@mans.de>
2021-01-26 16:49:43 +0100

    Refactor applyProps

--- src/util/applyProps.ts
+++ src/util/applyProps.ts
@@ -14,61 +14,57 @@ export const applyProps = (object: IStringIndexable, props: IStringIndexable) =>
     /* Attempt to parse the value */
     const parsed = parseJson(value)
 
-    switch (true) {
-      /* Skip ignored keys */
-      case IGNORED_KEYS.includes(key):
-        break
+    /* Skip all ignored keys. */
+    if (IGNORED_KEYS.includes(key)) return
 
-      /* Ignore all data- keys */
-      case firstKey.startsWith("data-"):
-        break
+    /* Skip all data attributes. */
+    if (firstKey.startsWith("data-")) return
 
-      /* Handle nested keys, ie. position-x */
-      case key in object && rest.length > 0:
-        applyProps(object[key], { [rest.join(".")]: value })
-        break
+    /* Handle nested keys, eg. position.x */
+    if (key in object && rest.length > 0)
+      return applyProps(object[key], { [rest.join(".")]: value })
 
-      /*
-      Handle boolean properties. We will check against the only values that we consider falsey here,
-      taking into account that they might be coming from string-based HTML element attributes, where a
-      stand-alone boolean attribute like "cast-shadow" will emit a value of "". Eh!
-      */
-      case typeof object[key] === "boolean":
-        object[key] = ![undefined, null, false, "no", "false"].includes(value)
-        break
-
-      /* Handle properties that provide .set methods */
-      case object[key]?.set !== undefined:
-        switch (true) {
-          /* If the value is an array, feed its destructured representation to the set method. */
-          case Array.isArray(parsed):
-            object[key].set(...parsed)
-            break
-
-          /* A bit of special handling for "scale" properties, where we'll also accept a single numerical value */
-          case key === "scale" && typeof parsed === "number":
-            object[key].setScalar(parsed)
-            break
-
-          /* If we have a parsed value, set it directly */
-          case parsed:
-            object[key].set(parsed)
-            break
-
-          /* Otherwise, set the original string value, but split by commas */
-          default:
-            const list = value.split(",").map((el: string) => parseFloat(el) || el)
-            object[key].set(...list)
-        }
-        break
-
-      default:
-        /*
-        If we've reached this point, we're finally able to set a property on the object.
-        Amazing! But let's only do it if the property key is actually known.
-        */
-        if (key in object) object[key] = parsed !== undefined ? parsed : value
+    /*
+    Handle boolean properties. We will check against the only values that we consider falsey here,
+    taking into account that they might be coming from string-based HTML element attributes, where a
+    stand-alone boolean attribute like "cast-shadow" will emit a value of "". Eh!
+    */
+    if (typeof object[key] === "boolean") {
+      object[key] = ![undefined, null, false, "no", "false"].includes(value)
+      return
     }
+
+    /* Handle properties that provide .set methods */
+    if (object[key]?.set !== undefined) {
+      /* If the value is an array, feed its destructured representation to the set method. */
+      if (Array.isArray(parsed)) {
+        object[key].set(...parsed)
+        return
+      }
+
+      /* A bit of special handling for "scale" properties, where we'll also accept a single numerical value */
+      if (key === "scale" && typeof parsed === "number") {
+        object[key].setScalar(parsed)
+        return
+      }
+
+      /* If we have a parsed value, set it directly */
+      if (parsed) {
+        object[key].set(parsed)
+        return
+      }
+
+      /* Otherwise, set the original string value, but split by commas */
+      const list = value.split(",").map((el: string) => parseFloat(el) || el)
+      object[key].set(...list)
+      return
+    }
+
+    /*
+    If we've reached this point, we're finally able to set a property on the object.
+    Amazing! But let's only do it if the property key is actually known.
+    */
+    if (key in object) object[key] = parsed !== undefined ? parsed : value
   }
 }
 
