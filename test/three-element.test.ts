2d1491a8efe7928b06695f75ee06f30a91ffbcb5
Hendrik Mans <hendrik@mans.de>
2021-01-27 16:41:09 +0100

    Clean up tests

---
+++ test/three-element.test.ts
@@ -0,0 +1,97 @@
+import { expect, fixture, html, nextFrame } from "@open-wc/testing"
+import { TemplateResult } from "lit-html"
+import * as THREE from "three"
+import "../src"
+import { ThreeElement } from "../src/ThreeElement"
+
+describe("<three-*> powered by ThreeElement", () => {
+  /*
+  Renders the given snippet in a three-game scaffolding and returns a reference
+  to the inner element.
+  */
+  const render = async <T extends Element = HTMLElement>(inner: TemplateResult) => {
+    const result = await fixture(html`<three-game><three-scene>${inner}</three-scene></three-game>`)
+    return result.querySelector("three-scene > :first-child") as T
+  }
+
+  const renderMeshElement = () => render<ThreeElement<THREE.Mesh>>(html`<three-mesh></three-mesh>`)
+
+  it("is backed by ThreeElement", async () => {
+    const el = await renderMeshElement()
+    expect(el).to.be.instanceOf(ThreeElement)
+  })
+
+  it("wraps a Three.js class", async () => {
+    const el = await renderMeshElement()
+    expect(el.object).to.be.instanceOf(THREE.Mesh)
+  })
+
+  describe("assigning to an attribute", () => {
+    it("sets the wrapped object's property of the same name", async () => {
+      const el = await renderMeshElement()
+
+      expect(el.object.name).to.equal("")
+
+      el.setAttribute("name", "A good mesh")
+      await nextFrame()
+
+      expect(el.object.name).to.equal("A good mesh")
+    })
+
+    it("supports dotty attributes (position.x)", async () => {
+      const el = await renderMeshElement()
+      el.object.position.x = 0
+      expect(el.object.position.x).to.equal(0)
+
+      el.setAttribute("position.x", "1")
+      await nextFrame()
+
+      expect(el.object.position.x).to.equal(1)
+    })
+
+    it("supports colon attributes (position:x)", async () => {
+      const el = await renderMeshElement()
+
+      el.object.position.x = 0
+      expect(el.object.position.x).to.equal(0)
+
+      el.setAttribute("position:x", "1")
+      await nextFrame()
+
+      expect(el.object.position.x).to.equal(1)
+    })
+
+    it("supports settable Vector3 attributes", async () => {
+      const el = await renderMeshElement()
+
+      el.object.position.set(0, 0, 0)
+      expect(el.object.position.x).to.equal(0)
+      expect(el.object.position.y).to.equal(0)
+      expect(el.object.position.z).to.equal(0)
+
+      el.setAttribute("position", "1, 2, 3")
+      await nextFrame()
+
+      expect(el.object.position.x).to.equal(1)
+      expect(el.object.position.y).to.equal(2)
+      expect(el.object.position.z).to.equal(3)
+    })
+
+    it("supports .setScalar attributes", async () => {
+      const el = await renderMeshElement()
+
+      el.object.scale.set(0, 0, 0)
+
+      expect(el.object.scale.x).to.equal(0)
+      expect(el.object.scale.y).to.equal(0)
+      expect(el.object.scale.z).to.equal(0)
+
+      el.setAttribute("scale", "1")
+      await nextFrame()
+
+      expect(el.object.scale.x).to.equal(1)
+      expect(el.object.scale.y).to.equal(1)
+      expect(el.object.scale.z).to.equal(1)
+    })
+  })
+})
