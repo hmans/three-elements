13f1d335bc9356c373c0f69b2e7bd4fb335dd26a
Hendrik Mans <hendrik@mans.de>
2021-01-29 20:20:33 +0100

    Cleanup

--- src/ThreeRendererElement.ts
+++ src/ThreeRendererElement.ts
@@ -4,9 +4,6 @@ import * as THREE from "three"
 export class ThreeRendererElement<T extends THREE.Renderer> extends ThreeElement<T> {
   connectedCallback() {
     super.connectedCallback()
-
-    console.log("Attaching renderer to game!")
     this.game!.renderer = this.object!
-    console.log("Attached!")
   }
 }
