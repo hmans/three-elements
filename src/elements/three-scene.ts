9c9cf876376017ad80fd76da46976a84ceb7a4f8
Hendrik Mans <hendrik@mans.de>
2021-01-30 03:40:42 +0100

    Fix build

--- src/elements/three-scene.ts
+++ src/elements/three-scene.ts
@@ -1,4 +1,4 @@
-import { Camera, Color, OrthographicCamera, PerspectiveCamera, Scene } from "three"
+import { Camera, Color, OrthographicCamera, PerspectiveCamera, Scene, WebGLRenderer } from "three"
 import { PointerEvents } from "../PointerEvents"
 import { ThreeElement } from "../ThreeElement"
 import { getThreeObjectBySelector } from "../util/getThreeObjectBySelector"
@@ -57,7 +57,9 @@ export class ThreeScene extends ThreeElement.for(Scene) {
   render() {
     const { renderer } = this.game
 
-    renderer.clearDepth()
+    if (renderer instanceof WebGLRenderer) {
+      renderer.clearDepth()
+    }
     renderer.render(this.object!, this.camera)
   }
 
