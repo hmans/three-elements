b2e6f25d3983733295854fe98ddbc4c1a6a8bd0c
Hendrik Mans <hendrik@mans.de>
2021-02-03 19:14:51 +0100

    Tweak WDS config

--- packages/examples/web-dev-server.config.mjs
+++ packages/examples/web-dev-server.config.mjs
@@ -1,4 +1,5 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild"
+import { importMapsPlugin } from "@web/dev-server-import-maps"
 
 export default {
   open: false,
@@ -6,13 +7,16 @@ export default {
   nodeResolve: true,
   rootDir: "public",
   plugins: [
-    // importMapsPlugin({
-    //   inject: {
-    //     importMap: {
-    //       imports: { "three-elements": "../three-elements/src/index.ts" }
-    //     }
-    //   }
-    // }),
+    importMapsPlugin({
+      inject: {
+        importMap: {
+          imports: {
+            "three-elements": "/__wds-outside-root__/2/three-elements/src/index.ts",
+            "@three-elements/proxy": "/__wds-outside-root__/2/proxy/src/index.ts"
+          }
+        }
+      }
+    }),
     esbuildPlugin({
       ts: true,
       target: "auto"
