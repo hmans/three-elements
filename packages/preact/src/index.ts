41a4e2540803a786e78829f70f8c1d36a4d3d21d
Hendrik Mans <hendrik@mans.de>
2021-02-03 21:59:24 +0100

    WIP type experiments

--- packages/preact/src/index.ts
+++ packages/preact/src/index.ts
@@ -1,6 +1,15 @@
 import { makeProxy } from "@three-elements/proxy"
 import { h } from "preact"
+import * as THREE from "three"
 
-export const T = makeProxy((tagName) => (props = null, children = []) =>
+type THREE = typeof THREE
+
+type PreactProxyFunction = (props?: any, children?: any[]) => any
+
+type PreactProxy = {
+  [K in keyof THREE]: PreactProxyFunction
+}
+
+export const T = (makeProxy((tagName) => (props = null, children = []) =>
   h(tagName, props, children)
-)
+) as unknown) as PreactProxy
