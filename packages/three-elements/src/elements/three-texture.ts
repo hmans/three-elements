8921d034ea844671b9a4daf89854ff65a501705f
Hendrik Mans <hendrik@mans.de>
2021-02-04 16:57:43 +0100

    Implement three-texture element

---
+++ packages/three-elements/src/elements/three-texture.ts
@@ -0,0 +1,36 @@
+import { Texture, TextureLoader } from "three"
+import { ThreeElement } from "../ThreeElement"
+import { registerThreeElement } from "../util/registerElement"
+
+export class ThreeTexture extends ThreeElement.for(Texture) {
+  private _url?: string
+
+  public get url() {
+    return this._url
+  }
+
+  public set url(url) {
+    /* If the URL has changed, initiate loading of texture. */
+    if (this._url !== url) {
+      if (url) {
+        this.object = new TextureLoader().load(url)
+      } else {
+        this.object = new Texture()
+      }
+    }
+
+    this._url = url
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    switch (name) {
+      case "url":
+        this.url = newValue
+        return true
+      default:
+        return super.attributeChangedCallback(name, oldValue, newValue)
+    }
+  }
+}
+
+registerThreeElement("three-texture", "Texture", ThreeTexture)
