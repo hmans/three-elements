2afe70d50514ff6ee1077f19159427eaa461fe94
Hendrik Mans <hendrik@mans.de>
2021-02-04 10:59:27 +0100

    Move some types over

--- packages/preact/src/index.ts
+++ packages/preact/src/index.ts
@@ -1,23 +1,11 @@
-import { makeProxy } from "@three-elements/proxy"
+import { makeProxy, ProxyAttributes } from "@three-elements/proxy"
 import { h } from "preact"
 import * as THREE from "three"
 
-/* Extract to proxy package: */
-
 type THREE = typeof THREE
 
-type ExposedThreeObjectProps<T> = {
-  [K in keyof T]: T[K] extends Function ? never : K
-}[keyof T]
-
-type ExposedThreeObjectAttributes<T> = {
-  [K in ExposedThreeObjectProps<T>]: T[K]
-}
-
-/* Preact specific: */
-
 type PreactProxyFunction<T> = (
-  props?: Partial<ExposedThreeObjectAttributes<T>>,
+  props?: Partial<ProxyAttributes<T>>,
   ...children: preact.ComponentChildren[]
 ) => preact.VNode
 
--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,5 +1,21 @@
 import { registeredThreeElements } from "three-elements"
 
+/**
+ * Returns property keys of a type T that do not represent
+ * functions.
+ */
+type NonFunctionProps<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K
+}[keyof T]
+
+/**
+ * Returns a partial type of T that only exposes properties
+ * that we want to expose via our proxies.
+ */
+export type ProxyAttributes<T> = {
+  [K in NonFunctionProps<T>]: T[K]
+}
+
 export const makeProxy = (fun: (tagName: string) => Function) =>
   new Proxy(registeredThreeElements, {
     get: (target, prop: string) => (target[prop] ? fun(target[prop]) : undefined)
