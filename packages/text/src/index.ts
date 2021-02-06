001e603f2072a928721ee60e31008638ca5db7f7
Hendrik Mans <hendrik@mans.de>
2021-02-06 14:03:27 +0100

    Move code to actual package

--- packages/text/src/index.ts
+++ packages/text/src/index.ts
@@ -1,1 +1,6 @@
-export const text = 123
+import { registerElement, ThreeElement } from "three-elements"
+import { Text as TroikaText } from "troika-three-text"
+
+export class ThreeText extends ThreeElement.for(TroikaText) {}
+
+registerElement("three-text", ThreeText)
