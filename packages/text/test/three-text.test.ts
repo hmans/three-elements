c2c06f6e1c9d3e4d509b554aedb31d8ef7f14475
Hendrik Mans <hendrik@mans.de>
2021-02-06 14:45:02 +0100

    Cleanup

--- packages/text/test/three-text.test.ts
+++ packages/text/test/three-text.test.ts
@@ -5,23 +5,13 @@ describe("three-text", () => {
   it("creates an element wrapping a Troika Text instance", async () => {
     await fixture(html`
       <three-game
-        ><three-scene> <three-text text="Hello World"></three-text> </three-scene
+        ><three-scene>
+          <three-text text="Hello World"></three-text> </three-scene
       ></three-game>
     `)
 
     const textElement = document.querySelector("three-text") as ThreeText
     expect(textElement).to.be.instanceOf(ThreeText)
     expect(textElement.object.text).to.eq("Hello World")
-  })
-
-  it("assigns the color attribute correctly", async () => {
-    await fixture(html`
-      <three-game
-        ><three-scene> <three-text text="Hello World" color="red"></three-text> </three-scene
-      ></three-game>
-    `)
-
-    const textElement = document.querySelector("three-text") as ThreeText
-    expect(textElement.object.material.color.getHexString()).to.eq("ff0000")
   })
 })
